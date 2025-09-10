e0df20f403f08b3dfaeb2ce904edb299de29d364
JustFr4nck <francyperry07@gmail.com>
2025-09-10 17:55:50 +0200

    starting homepage

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/"></RouterLink>
   <RouterView />
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <main>
-    
-  </main>
+  <div class="flex justify-center items-center w-full bg-blue-300 h-screen">
+    <div class="bg-white flex w-100 h-100 justify-center">
+      <h1 class="">Let's discover the world of Pokemon</h1>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts"></script>
