f4514ad2e9c9a505d33e75078c2b90e92850ae22
JustFr4nck <francyperry07@gmail.com>
2025-09-11 10:43:16 +0200

    footer added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import MyFooter from './components/MyFooter.vue';
 </script>
 
 <template>
   <RouterLink to="/"></RouterLink>
+  
+  
   <RouterView />
+  <MyFooter/>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="flex justify-center items-center w-full bg-blue-300 h-screen">
-    <div class="bg-white flex w-100 h-100 justify-center">
-      <h1 class="">Let's discover the world of Pokemon</h1>
+  <div class="flex justify-center items-center w-full bg-black h-screen">
+    <div class="bg-white flex w-100 h-100 justify-center items-center crazyBox flex-col">
+      <h1 class="bungee-spice-regular text-center">Let's discover the world of Pokemon</h1>
+      <button class=" mt-5 dexBtn">Pokedex</button>
     </div>
   </div>
 </template>
