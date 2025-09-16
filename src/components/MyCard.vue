a4206004d6599c946c1f6f08f1781578ec54ff9d
JustFr4nck <francyperry07@gmail.com>
2025-09-16 10:12:37 +0200

    cards release

--- src/components/MyCard.vue
+++ src/components/MyCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="card card-side bg-base-100 shadow-sm m-5">
     <figure>
-      <img src="../../public/icon/pokemon-trainer.png" alt="Movie" />
+      <img :src="img" alt="Movie" />
     </figure>
     <div class="card-body">
-      <h2 class="card-title">{{ pokemon }}</h2>
-      <p>{{description}}</p>
+      <h2 class="card-title">{{ poke.name }}</h2>
+      <p>description</p>
       <div class="card-actions justify-end">
         <button class="btn btn-primary">See more</button>
       </div>
@@ -14,8 +14,26 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue'
 
-const pokemon = ref("Pokemon");
-const description = ref("Description of a pokemon")
+const img = ref("");
+
+const props = defineProps({
+  poke: {
+    type: Object,
+    required: true,
+  },
+})
+
+async function printSprite() {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${props.poke.name}`)
+  const data = await response.json()
+  img.value = data.sprites.front_default
+}
+
+onMounted(() =>{
+
+  printSprite();
+})
+
 </script>
--- src/views/Pokedex.vue
+++ src/views/Pokedex.vue
@@ -3,36 +3,9 @@
     <MyNavbar />
   </nav>
   <main>
-    <div class="flex flex-col">
-      <div class="flex flex-row mt-20 mb-20">
-        <MyCard class="basis-1/4" />
-        <MyCard class="basis-1/4" />
-        <MyCard class="basis-1/4" />
-        <MyCard class="basis-1/4" />
-      </div>
-      <div class="flex flex-row mt-20 mb-20">
-        <MyCard class="basis-1/4" />
-        <MyCard class="basis-1/4" />
-        <MyCard class="basis-1/4" />
-        <MyCard class="basis-1/4" />
-      </div>
-      <div class="flex flex-row mt-20 mb-20">
-        <MyCard class="basis-1/4" />
-        <MyCard class="basis-1/4" />
-        <MyCard class="basis-1/4" />
-        <MyCard class="basis-1/4" />
-      </div>
-      <div class="flex flex-row mt-20 mb-20">
-        <MyCard class="basis-1/4" />
-        <MyCard class="basis-1/4" />
-        <MyCard class="basis-1/4" />
-        <MyCard class="basis-1/4" />
-      </div>
-      <div class="flex flex-row mt-20 mb-20">
-        <MyCard class="basis-1/4" />
-        <MyCard class="basis-1/4" />
-        <MyCard class="basis-1/4" />
-        <MyCard class="basis-1/4" />
+    <div class="">
+      <div class="flex flex-row flex-wrap mt-20 mb-20 justify-center">
+        <MyCard class="basis-1/5" v-for="p in pokemon" :poke="p" />
       </div>
     </div>
   </main>
@@ -41,4 +14,19 @@
 <script setup>
 import MyCard from '@/components/MyCard.vue'
 import MyNavbar from '@/components/MyNavbar.vue'
+import { onMounted, ref } from 'vue'
+
+const pokemon = ref([])
+const description = ref('Description of a pokemon')
+
+async function getPokemon() {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+  const data = await response.json()
+  pokemon.value = data.results
+
+}
+
+onMounted(() => {
+  getPokemon()
+})
 </script>
